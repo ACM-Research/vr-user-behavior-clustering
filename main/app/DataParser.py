77eba32610b3ed72980402c5d1b002fbc03aecd8
Thinh Nguyen <ngpthinh11@gmail.com>
2021-09-27 19:53:20 -0500

    heatmap creation

--- main/app/DataParser.py
+++ main/app/DataParser.py
@@ -1,5 +1,9 @@
 import app.env
 import math, csv, os, glob
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from typing import List
 from pathlib import PureWindowsPath
 from pathlib import Path
@@ -15,6 +19,8 @@ class DataParser:
         self.allUserID = DataParser.find_csv_filenames(self.csvPath)
         self.frameImgWidth = env.IMAGE_WIDTH
         self.frameImgHeigth = env.IMAGE_HEIGHT
+        self.userTraces = []
+        self.heatmapArray = None
 
         # print(self.frameImgHeigth)
         # print(self.frameImgWidth)
@@ -36,36 +42,46 @@ class DataParser:
             yield file.replace(suffix, "")
 
     @staticmethod
-    def csvDictReader(path_to_dir, fileName,suffix=".csv"):
+    def csvRowReader(path_to_dir, rowIndex, fileName, suffix=".csv"):
         path_to_dir = Path(f"{path_to_dir}/{fileName}{suffix}")
-        with open(path_to_dir, "r") as f:
-            reader = csv.DictReader(f)
-            a = list(reader)
-            print(a)
+        with open(path_to_dir, "r") as file:
+            csv_reader = csv.reader(file)
+            rows = list(csv_reader)
+            return rows[rowIndex]
 
-    @classmethod
-    def convertUserTraces(self, frameNumber):
+    def collectHeatDiagramLocations(self, frameNumber):
         # draw user trace points
-        self.usertraces = []
         colwidth = self.frameImgWidth / self.numCols
         rowheight = self.frameImgHeigth / self.numRows
         
         for userid in self.allUserID:
+            frameRow = DataParser.csvRowReader(self.csvPath, frameNumber - 1, userid)
+            _3dVector = [float(frameRow[5]), float(frameRow[6]), float(frameRow[7])]
+            
+            # convert 3d vector to 2d vector
+            (yaw, pitch) = self.convvec2angl(_3dVector)
+            x = ((yaw + 180) / 360) * self.frameImgWidth
+            y = ((90 - pitch) / 180) * self.frameImgHeigth
 
-            trace_row = frameNumber - 1
-            arr = [trace_row[5], trace_row[6], trace_row[7]]
-            yaw, pitch = self.convvec2angl(arr)
-            """ 
-            !! 
-            How does the calculation work? 
-            What is x, y here compared to x, y, z in the arr vector? 
-            Why do we need this value? 
-            !! 
-            """
-            x = ((yaw + 180) / 360) * self.imagesize[0]
-            y = ((90 - pitch) / 180) * self.imagesize[1]
-
-            # ?? Why might parsing the x, y values into indices here be useful ??
             x_index = int(x / colwidth)
             y_index = int(y / rowheight)
-            self.usertraces.append((userid, frameNumber, (x_index, y_index)))
+            self.userTraces.append((userid, frameNumber, (x_index, y_index)))
+
+        print(self.userTraces)
+
+    def loadHeatmapData(self):
+        self.heatmapArray = np.empty(shape=(self.numRows, self.numCols))
+        self.heatmapArray.fill(0)
+        
+        # # row 2 col 3
+        # x[3-1][2-1] = x[3-1][2-1] + 1
+        
+        for user in self.userTraces:
+            self.heatmapArray[user[2][1]][user[2][0]] = self.heatmapArray[user[2][1]][user[2][0]] + 1
+
+        print(self.heatmapArray)
+
+    def loadHeatmap(self):
+        ax = sns.heatmap(self.heatmapArray)
+        plt.show()
+        
--- main/vr_usr_bhvr.py
+++ main/vr_usr_bhvr.py
@@ -5,5 +5,9 @@ from pathlib import PureWindowsPath
 
 parser = DataParser(os.getcwd(), 1, 20, 10)
 
-path = r"D:\Users\Thinh Nguyen\OneDrive\_1_Fall2021\ACM\Research\vr-user-behavior\main\resources\UserTracesByVideo\1"
-DataParser.csvDictReader(path, "0Z4VWJ")
+# path = r"C:\Users\Thinh Nguyen\OneDrive\_1_Fall2021\ACM\Research\vr-user-behavior\main\resources\UserTracesByVideo\1"
+# DataParser.csvRowReader(path, 0, "0Z4VWJ")
+
+parser.collectHeatDiagramLocations(1)
+parser.loadHeatmapData()
+parser.loadHeatmap()
